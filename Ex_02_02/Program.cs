93366600f1e057a12a6728eb83a151eea8cf11cb
zabaglione <z.zabaglione@gmail.com>
2026-04-27 15:43:17 +0900

    Finished Ex_02_02

---
+++ Ex_02_02/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Ex_02_02;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("文字列を入力してください->");
+        string value = Console.ReadLine();//変数valueに入力された値を代入
+        Console.WriteLine( value ); //入力された値を表示
+    }
+}
