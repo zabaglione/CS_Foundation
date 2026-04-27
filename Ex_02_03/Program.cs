5a455e6211d129ce55a53b33dad94b5fc8a3b4fa
zabaglione <z.zabaglione@gmail.com>
2026-04-27 16:48:11 +0900

    Ex_02_03スタート

---
+++ Ex_02_03/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Ex_02_03;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
--- Example/Program.cs
+++ Example/Program.cs
@@ -4,11 +4,19 @@ static class Program
 {
     static void Main(string[] args)
     {
-        int a = 5;
-        int b = 2;
-        float c = a / (float)b;
-
-        System.Console.Write("c=");
-        System.Console.WriteLine(c);
+        bool a = true, b = false;
+        Console.WriteLine($"a && a = {a && a}");
+        Console.WriteLine($"a && b = {a && b}");
+        Console.WriteLine($"b && a = {b && a}");
+        Console.WriteLine($"b && b = {b && b}");
+        Console.WriteLine($"a || a = {a || a}");
+        Console.WriteLine($"a || b = {a || b}");
+        Console.WriteLine($"b || a = {b || a}");
+        Console.WriteLine($"b || b = {b || b}");
+        int x = 1;
+        Console.WriteLine($"b && x++ > 1 = {b && x++ > 1}");
+        Console.WriteLine($"x = {x}");
+        Console.WriteLine($"b & x++ > 1 = {b & x++ > 1}");
+        Console.WriteLine($"x = {x}");
     }
 }
