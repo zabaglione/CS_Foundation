c7761b0570973cf3bad91cde22471277c0d26e21
zabaglione <z.zabaglione@gmail.com>
2026-04-28 14:05:12 +0900

    Ex_02_12の準備

---
+++ Ex_02_12/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Ex_02_12;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
--- Example/Program.cs
+++ Example/Program.cs
@@ -4,14 +4,23 @@ static class Program
 {
     static void Main(string[] args)
     {
-        for (int i = 0; i < 6; i++)
+        string day = "本日";
+        string wether = "晴れ";
+        // 可変数の引数を渡せる
+        StringConcatenation(day, "は", wether, "です。");
+        /// <summary>
+        /// 引数の文字列を結合して表示する
+        /// </summary>
+        /// <param name="strings">結合する文字列</param>
+        static void StringConcatenation(params string[] strings)
         {
-            if (i == 2)
+            var result = "";
+            for (int i = 0; i < strings.Length; i++)
             {
-                continue;
+                result += strings[i];
             }
-            Console.WriteLine($"i = {i}");
+            Console.WriteLine(result);
         }
-        Console.WriteLine("終了");
     }
+
 }
