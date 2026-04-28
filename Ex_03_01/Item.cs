6bf298ca76469b48b1256ca65b57b5dd6311c913
zabaglione <z.zabaglione@gmail.com>
2026-04-28 14:53:37 +0900

    Ex_03_01の準備

---
+++ Ex_03_01/Item.cs
@@ -0,0 +1,9 @@
+namespace Ex_03_01;
+
+/// <summary>
+/// 商品情報を表すクラス
+/// </summary>
+public class Item
+{
+
+}
---
+++ Ex_03_01/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Ex_03_01;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
